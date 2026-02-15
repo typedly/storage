9cd877617be7f6d0460e9c12527b389f1d3a25df
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-02-15 15:24:52 +0000

    feat(`StorageAdapter`): add interface extending `StorageShape` with version property

---
+++ src/lib/interface/storage.adapter.ts
@@ -0,0 +1,21 @@
+import { StorageShape } from "./storage-shape.interface";
+/**
+ * @description
+ * @export
+ * @interface StorageAdapter
+ * @template [O=Record<string, any>] 
+ * @template {keyof O} [K=keyof O] 
+ * @template [V=O[K]] The type of the values in the storage.
+ * @template {boolean} [R=false] 
+ * @template {{key: K, value?: V}} [P={key: K, value?: V}] 
+ * @extends {StorageShape<R, O, K, V, P>}
+ */
+export interface StorageAdapter<
+  O = Record<string, any>,
+  K extends keyof O = keyof O,
+  V = O[K],
+  R extends boolean = false,
+  P extends {key: K, value?: V} = {key: K, value?: V}
+> extends StorageShape<R, O, K, V, P> {
+  version: string;
+}
